d2227ff88ff9e7afd3b02d3d302a04dede217acf
Jin Li <lijin110110@gmail.com>
2021-05-29 18:24:11 +0800

    Reconstruct router with trie implementation

--- src/bottle/router.go
+++ src/bottle/router.go
@@ -3,20 +3,79 @@ package bottle
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
+	// trie roots for "GET", "POST", etc...
+	roots map[string]*node
+	// handlers for all routes
 	handlers map[string]HandlerFunc
 }
 
+// examples for router:
+// roots['GET'], roots['POST']
+// handlers['GET-/p/:lang/doc'], handlers['POST-/p/book']
+
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandlerFunc)}
+	return &router{
+		roots: make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+func parse(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
 
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
 	log.Printf("Route %4s - %s", method, pattern)
+	parts := parse(pattern)
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	key := method + "-" + pattern
 	r.handlers[key] = handler
+}
+
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	realParts := parse(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(realParts, 0)
+
+	if n != nil {
+		parts := parse(n.pattern)
+		for i, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = realParts[i]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(realParts[i:], "/")
+				break
+			}
+			return n, params
+		}
+	}
+
+	return nil, nil
 }
 
 func (r *router) handle(c *Context) {
