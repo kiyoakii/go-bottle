196916304ab4e12792a9085fb7f08979d255d066
Jin Li <lijin110110@gmail.com>
2021-05-29 13:20:09 +0800

    Add a simple driver

---
+++ src/main.go
@@ -0,0 +1,48 @@
+package main
+
+/*
+(1)
+$ curl -i http://localhost:9999/
+HTTP/1.1 200 OK
+Date: Mon, 12 Aug 2019 16:52:52 GMT
+Content-Length: 18
+Content-Type: text/html; charset=utf-8
+<h1>Hello bottle</h1>
+
+(2)
+$ curl "http://localhost:9999/hello?name=jin"
+hello jin, you're at /hello
+
+(3)
+$ curl "http://localhost:9999/login" -X POST -d 'username=jinli&password=1234'
+{"password":"1234","username":"jinli"}
+
+(4)
+$ curl "http://localhost:9999/xxx"
+404 NOT FOUND: /xxx
+*/
+
+import (
+	"bottle"
+	"net/http"
+)
+
+func main() {
+	r := bottle.New()
+	r.GET("/", func(c *bottle.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello bottle</h1>")
+	})
+	r.GET("/hello", func(c *bottle.Context) {
+		// expect /hello?name=jin
+		c.Text(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *bottle.Context) {
+		c.JSON(http.StatusOK, bottle.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
+}
